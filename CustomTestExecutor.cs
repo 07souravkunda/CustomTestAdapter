cf0ebf7633fccaafd738755b3e76096912f2992a
Sourav Kunda <50537259+07souravkunda@users.noreply.github.com>
2023-01-03 16:54:11 +0530

    custom executor

---
+++ CustomTestExecutor.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel.Adapter;
+
+namespace Custom.VisualStudio.TestAdapter
+{
+    [ExtensionUri("executor://NUnit3TestExecutor")]
+    public class BStackTestExecutor : ITestExecutor
+    {
+        public void Cancel()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RunTests(IEnumerable<TestCase>? tests, IRunContext? runContext, IFrameworkHandle? frameworkHandle)
+        {
+            var shouldRun = Environment.GetEnvironmentVariable("RUN_CUSTOM_EXECUTOR");
+            if (shouldRun == "true")
+            {
+                // Do some custom execution
+            }
+            else
+            {
+                // Skip to default nunit3 executor
+            }
+        }
+     
+        void ITestExecutor.RunTests(IEnumerable<string>? sources, IRunContext? runContext, IFrameworkHandle? frameworkHandle)
+        {
+            // 
+        }
+    }
+}
